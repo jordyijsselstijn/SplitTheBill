946c0400b97168a124198566275d80ad75568e4a
Jordy IJsselstijn <jordyijsselstijn@me.com>
2015-06-05 18:04:19 +0200

    Initial Commit

---
+++ SplitTheBill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplitTheBill
+//
+//  Created by Jordy IJsselstijn on 05-06-15.
+//  Copyright (c) 2015 jordyijsselstijn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
